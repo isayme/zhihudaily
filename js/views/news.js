5163ce1bd45526c0987e5aa3b83c30d5ec66557f
iSayme <isaymeorg@gmail.com>
2015-03-23 15:48:51 +0800

    when ajax failed, show message

--- js/views/news.js
+++ js/views/news.js
@@ -11,11 +11,12 @@ define([
     template: _.template($('#news-template').html()),
 
     initialize: function() {
+      this.renderLoading();
       this.listenTo(newsCollection, 'reset', this.update_news);
-      //newsCollection.fetch();
     },
     
     update_news: function(collection) {
+      this.$el.html('');
       for (var i = 0, l = collection.length; i < l; i++) {
         var model = collection.at(i);
         this.$el.append(this.template(model.attributes));
@@ -25,10 +26,34 @@ define([
     },
 
     render: function(year, month, day) {
-      newsCollection.fetch({reset: true, url: newsCollection.url(year, month, day)})
-      this.$el.html('');
+      this.renderLoading();
+      newsCollection.fetch({
+        reset: true,
+        url: newsCollection.url(year, month, day),
+        error: this.renderError.bind(this)
+      });
       
       return this;
+    },
+    
+    renderError: function(collection, resp, options) {
+      if (resp.status === 404) {
+        this.$el.html(
+          '<div class="error">\
+            <h2>Not Found</h2>\
+            <p>呀, 等一等呗, 这天的数据还没同步呢!</p>\
+          </div>');
+      } else {
+        this.$el.html(
+          '<div class="error">\
+            <h2>WHOOOPS~</h2>\
+            <p>嗯, 检查你的网络先!</p>\
+          </div>');
+      }
+    },
+    
+    renderLoading: function() {
+      this.$el.html('<div class="loading"><img src="img/loading-bubbles.svg" /></div>');
     }
   });
 
